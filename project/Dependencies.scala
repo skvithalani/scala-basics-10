384a158435eb3887506fae83092882387063ac0d
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-01-31 16:46:00 +0530

    correction

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
-  val `play-json` = "com.typesafe.play" %% "play-json" % "2.5.12"
+  val `play-json` = "com.typesafe.play" %% "play-json" % "2.6.0-M1"
 }
